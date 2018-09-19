d493a345848f77588965d36b819fc83f42b5db27
Kira Hartlage <kira.hartlage@gmail.com>
2018-09-19 17:47:02 -0400

    doing more data stuff

--- SoccerStats/Program.cs
+++ SoccerStats/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Collections.Generic;
 
 namespace SoccerStats
 {
@@ -10,12 +11,7 @@ namespace SoccerStats
             string currentDirectory = Directory.GetCurrentDirectory();
             DirectoryInfo directory = new DirectoryInfo(currentDirectory);
             var fileName = Path.Combine(directory.FullName, "SoccerGameResults.csv");
-            var fileContents = ReadFile(fileName);
-            string[] fileLines = fileContents.Split(new char[] { '\n' });
-            foreach(var line in fileLines)
-            {
-                Console.WriteLine(line);
-            }
+            var fileContents = ReadSoccerResults(fileName);
         }
 
         public static string ReadFile(string fileName)
@@ -24,6 +20,52 @@ namespace SoccerStats
             {
                 return reader.ReadToEnd();
             }
+        }
+
+        public static List<GameResult> ReadSoccerResults(string fileName)
+        {
+            var soccerResults = new List<GameResult>();
+            using(var reader = new StreamReader(fileName))
+            {
+                string line = "";
+                reader.ReadLine(); // reads first line, i.e. 'throws it away'
+                while((line = reader.ReadLine()) != null)
+                {
+                    var gameResult = new GameResult();
+                    string[] values = line.Split(',');
+
+                    DateTime gameDate;
+                    if(DateTime.TryParse(values[0], out gameDate))
+                    {
+                        gameResult.GameDate = gameDate;
+                    }
+                    gameResult.TeamName = values[1];
+                    HomeOrAway homeOrAway;
+                    if(Enum.TryParse(values[2], out homeOrAway))
+                    {
+                        gameResult.HomeOrAway = homeOrAway;
+                    }
+                    int parseInt;
+                    if(int.TryParse(values[3], out parseInt))
+                    {
+                        gameResult.Goals = parseInt;
+                    }
+                    if (int.TryParse(values[4], out parseInt))
+                    {
+                        gameResult.GoalAttempts = parseInt;
+                    }
+                    if (int.TryParse(values[5], out parseInt))
+                    {
+                        gameResult.ShotsOnGoal = parseInt;
+                    }
+                    if (int.TryParse(values[6], out parseInt))
+                    {
+                        gameResult.ShotsOffGoal = parseInt;
+                    }
+                    soccerResults.Add(gameResult);
+                }
+            }
+            return soccerResults;
         }
     }
 }
